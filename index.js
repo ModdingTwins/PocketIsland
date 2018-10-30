9ef5b7c09879394d658130bb984b601367691e9a
MTwins <42128152+ModdingTwins@users.noreply.github.com>
2018-10-29 21:40:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const bot = new Discord.Client()
 bot.on("ready", () => {
 console.log(`${bot.user.username}` + "is online")
 
-bot.user.setActivity("TwinzFactions", {type: "STREAMING"})
+bot.user.setActivity("TwinFactions.tk | -help", {type: "STREAMING"})
 });
 bot.login(process.env.TOKEN)
 
 bot.on("message", (message) => {
-if(message.content === ".ip") {
+if(message.content === "-ip") {
 message.channel.send("twinfactions.tk : 19132")
 }
 });
 
 bot.on("message", (message) => {
-if(message.content === ".youtube") {
+if(message.content === "-youtube") {
 message.channel.send("YouTube.com/c/ModdingTwinz")
 }
 });
