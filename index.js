2ca93927553c3687c0df70fd3c6358a981fcf598
MTwins <42128152+ModdingTwins@users.noreply.github.com>
2018-10-29 21:14:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const Discord = require("discord.js")
 const bot = new Discord.Client()
+bot.on("ready", () => {
+console.log(`${bot.user.username}` + "is online")
 
+bot.user.setActivity("TwinzFactions", {type: "STREAMING"})
+});
 bot.login(process.env.TOKEN)
