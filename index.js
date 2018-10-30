80cf0383153e96753287e87a7401d6fab6ef9dd0
MTwins <42128152+ModdingTwins@users.noreply.github.com>
2018-10-29 23:27:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,30 +3,30 @@ const bot = new Discord.Client()
 bot.on("ready", () => {
 console.log(`${bot.user.username}` + "is online")
 
-bot.user.setActivity("TwinFactions.tk | +help", {type: "STREAMING"})
+bot.user.setActivity("TwinFactions.tk | ¿help", {type: "STREAMING"})
 });
 bot.login(process.env.TOKEN)
 
 bot.on("message", (message) => {
-if(message.content === "+ip") {
+if(message.content === "¿ip") {
 message.channel.send("twinfactions.tk : 19132")
 }
 });
 
 bot.on("message", (message) => {
-if(message.content === "+youtube") {
+if(message.content === "¿youtube") {
 message.channel.send("YouTube.com/c/ModdingTwinz")
  }
  });
  
 bot.on("message", (message) => {
-if(message.content === "+discord") {
+if(message.content === "¿discord") {
 message.channel.send("https://discord.gg/u3vqvst")
 }
 });
  
 bot.on("message", (message) => {
-if(message.content === "+help") {
+if(message.content === "¿help") {
 console.log("Log")
 message.channel.send(`${message.author.username}` + ", I Have Sent You The Commands 📪")
 message.author.send("Please specify a module that you would like to look up:\n -ip (TwinFactions)\n -youtube (ModdingTwinz)\n -discord (TwinFactions)\n\n I was created by ``ModdingTwinz#4560`` & also this is beta 1.0")
